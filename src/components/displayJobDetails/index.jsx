79259794d3bdaeda75acdf76e8c4c8c31da28578
Akshay <akshayshinde61493@gmail.com>
2024-07-03 22:56:51 +0530

    this is last page

--- src/components/displayJobDetails/index.jsx
+++ src/components/displayJobDetails/index.jsx
@@ -5,31 +5,6 @@ import { useEffect, useState } from "react";
 import { FaStar, FaBriefcase } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 import { AlertLink } from "react-bootstrap";
-// {job_details: {…}, similar_jobs: Array(3)}
-// job_details
-//  :  "https://assets.ccbp.in/frontend/react-js/jobby-app/netflix-img.png"
-// company_website_url : "https://about.netflix.com/en"
-// employment_type: "Internship"
-// id:"bb95e51b-b1b2-4d97-bee4-1d5ec2b96751"
-// job_description: "We are looking for a DevOps Engineer with a minimum of 5 years of industry experience, preferably working in the financial IT community. The position in the team is focused on delivering exceptional services to both BU and Dev partners to minimize/avoid any production outages. The role will focus on production support."
-// life_at_company : {description: 'Our core philosophy is people over process. Our cu…us common ground. We want to entertain the world.', image_url: 'https://assets.ccbp.in/frontend/react-js/jobby-app/life-netflix-img.png'}
-// location: "Delhi"
-// package_per_annum:  "10 LPA"
-// rating :  4
-
-// title: "Devops Engineer"
-
-// company_logo_url: "https://assets.ccbp.in/frontend/react-js/jobby-app/netflix-img.png"
-// employment_type: "Freelance"
-// id: "2b40029d-e5a5-48cc-84a6-b6e12d25625d"
-// job_description:  "The Experimentation Platform team builds internal tools with a big impact across the company. We are looking to add a UI engineer to our team to continue to improve our experiment analysis workflow and tools. Ideal candidates will be excited by direct contact with our users, fast feedback, and quick iteration."
-// location: "Delhi"
-// rating : 4
-// title: "Frontend Engineer"
-
-// 1: {id: '3cc666e5-23a5-4981-ade2-61115f95ac0b', title: 'Frontend Engineer', company_logo_url: 'https://assets.ccbp.in/frontend/react-js/jobby-app/netflix-img.png', location: 'Mumbai', employment_type: 'Part Time', …}
-// 2: {id: '96e1ddb6-930c-4ca4-a80e-998ce410c6cf', title: 'Frontend Engineer', company_logo_url: 'https://assets.ccbp.in/frontend/react-js/jobby-app/netflix-img.png', location: 'Bangalore', employment_type: 'Freelance', …}
-// length: 3
 
 const DisplayJobDetails = () => {
   const [allValue, setValue] = useState({
